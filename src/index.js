cf4ceb9400cc0a3f22caf56dab24a7d1c85874d2
曾俊坤 <zengjunkun@cvte.com>
2021-04-21 20:45:26 +0800

    add:webpack HMR

---
+++ src/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    const beforeCanvas = document.querySelector("#before");
+    const afterCanvas = document.querySelector("#after");
+    const beforeCtx = beforeCanvas.getContext("2d");
+    const afterCtx = afterCanvas.getContext("2d");
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (e) {
+      beforeCanvas.width = 200;
+      beforeCanvas.height = 200;
+      afterCanvas.width = 200;
+      afterCanvas.height = 200;
+      const img = new Image();
+      img.src = e.target.result;
+      img.onload = function () {
+        beforeCtx.drawImage(img, 0, 0, 200, 200);
+        // const loadBase64 = beforeCanvas.toDataURL();
+        // 2gery
+        const imageData = beforeCtx.getImageData(0, 0, 200, 200);
+        const { data } = imageData;
+        for (let i = 0; i < data.length; i += 4) {
+          const grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
+          data[i] = grey;
+          data[i + 1] = grey;
+          data[i + 2] = grey;
+        }
+                    console.log('imageData', imageData);
+        afterCtx.putImageData(imageData, 0, 0);
+      };
+    };
+  });
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: {
+      import: './src/index.js',
+      dependOn: 'lodash'
+    },
+    lodash: 'lodash'
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: '图像处理',
+      template: 'src/assets/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      Assets: path.resolve(__dirname, 'src/assets'),
+      Lib: path.resolve(__dirname, 'src/lib'),
+      Plugins: path.resolve(__dirname, 'src/plugins')
+    }
+  }
+}
